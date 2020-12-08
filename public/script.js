4cea17600eb0ccf67a0ca9224930c0b7040e2172
Emilis Sapronas <esapronas@hotmail.com>
2020-12-08 11:14:42 +0000

    add test task description

--- public/script.js
+++ public/script.js
@@ -6,4 +6,18 @@ $(document).ready(function() {
       $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
       $(this).addClass('active');
   });
+
+
+  // Widget configuration
+  let configuration = {
+    attributes: ['red', 'blue', 'black'],
+    placeholder: '.product-color',
+    placeholder_text: 'Surprise me with the color',
+    cart_button: '.cart-btn',
+    image: window.location.origin + '/images/black.png',
+    select_attribute: function(attr) {}
+  }
+
+  // let widget = Widget(configuration)
+  // setTimeout(widget.start, 2000)
 });
